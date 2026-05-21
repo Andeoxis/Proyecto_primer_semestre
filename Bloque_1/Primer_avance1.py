a97ed6746e318d9291d61516a7882186b1a7067a
Anthony Leandro Vasquez Torrico <vasqueztorricoanthonyleandro@gmail.com>
2026-05-20 21:37:25 -0400

    Actializacion del documento 1

--- Bloque_1/Primer_avance1.py
+++ Bloque_1/Primer_avance1.py
@@ -1,9 +1,10 @@
+# Aca lo que hago es dar la bienvenida al usuario y la \n la uso para saltar lineas, depende si lo coloco atras o adalante salta un espacio arriba o abajo.
 print('\n          BIENVENIDO AL SISTEMA DE REGISTRO DE HUEVOS POR MAYOR          \n')
-
+# Aca estoy guardando cual es la contraseña correcta que permitira entrar al sistema.
 clave_de_acceso = 'Holabebe'
 
 
-
+ # Aca estamos usando el while True, ya que este hace que el programa se ejecute de manera indefinida hasta que se cumpla la condicion de romper el ciclo, en este caso es cuando el usuario ingresa la contraseña correcta, si no es asi, el programa seguira pidiendo la contraseña y mostrando un mensaje de error.
 while True:
     Ingreso_de_contraseña = input('''Ingrese la clave de acceso:
 ''')
@@ -12,6 +13,6 @@ while True:
         break
     else:
         print('Clave de acceso incorrecta. Intente nuevamente.')
-        
+
 
 
