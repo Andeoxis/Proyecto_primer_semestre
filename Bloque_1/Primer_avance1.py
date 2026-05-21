5e9ee86d12dc08121a33963d3ccb03453384de7a
Anthony Leandro Vasquez Torrico <vasqueztorricoanthonyleandro@gmail.com>
2026-05-20 21:30:46 -0400

    Actializacion del documento

--- Bloque_1/Primer_avance1.py
+++ Bloque_1/Primer_avance1.py
@@ -1,1 +1,17 @@
 print('\n          BIENVENIDO AL SISTEMA DE REGISTRO DE HUEVOS POR MAYOR          \n')
+
+clave_de_acceso = 'Holabebe'
+
+
+
+while True:
+    Ingreso_de_contraseña = input('''Ingrese la clave de acceso:
+''')
+    if Ingreso_de_contraseña == clave_de_acceso:
+        print('Acceso permitido')
+        break
+    else:
+        print('Clave de acceso incorrecta. Intente nuevamente.')
+        
+
+
