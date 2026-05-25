65d3ff6bcd12c4b57c278746e20f7ed7a146e97a
Anthony Leandro Vasquez Torrico <vasqueztorricoanthonyleandro@gmail.com>
2026-05-25 08:54:11 -0400

    second commit

--- Bloque_1/Primer_avance1.py
+++ Bloque_1/Primer_avance1.py
@@ -1,13 +1,19 @@
 # Aca lo que hago es dar la bienvenida al usuario y la \n la uso para saltar lineas, depende si lo coloco atras o adalante salta un espacio arriba o abajo.
 print('\n          BIENVENIDO AL SISTEMA DE REGISTRO DE HUEVOS POR MAYOR          \n')
 # Aca estoy guardando cual es la contraseña correcta que permitira entrar al sistema.
-clave_de_acceso = 'Holabebe'
 
 
- # Aca estamos usando el while True, ya que este hace que el programa se ejecute de manera indefinida hasta que se cumpla la condicion de romper el ciclo, en este caso es cuando el usuario ingresa la contraseña correcta, si no es asi, el programa seguira pidiendo la contraseña y mostrando un mensaje de error.
+usuario = input('''Cree un nombre de usuario:
+''').strip()
+clave_de_acceso = input('''Crea una clave de acceso:
+''').strip()
+print(f'Bienvenido usuario {usuario}, su clave de acceso ha sido creada exitosamente. Por favor, ingrese su clave de acceso para ingresar al sistema.')
+
+
+# Aca estamos usando el while True, ya que este hace que el programa se ejecute de manera indefinida hasta que se cumpla la condicion de romper el ciclo, en este caso es cuando el usuario ingresa la contraseña correcta, si no es asi, el programa seguira pidiendo la contraseña y mostrando un mensaje de error.
 while True:
     Ingreso_de_contraseña = input('''Ingrese la clave de acceso:
-''')
+''').strip()  # El .strip() se utiliza para eliminar cualquier espacio en blanco al inicio o al final de la cadena ingresada por el usuario.
     if Ingreso_de_contraseña == clave_de_acceso:
         print('Acceso permitido')
         break
